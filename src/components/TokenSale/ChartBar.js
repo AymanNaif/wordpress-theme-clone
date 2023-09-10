187a3e709b8db49eb8d61a11cd65424a8e3a762a
Ayman Naif <ayman@zenhr.com>
2023-09-10 12:17:08 +0300

    finish tokenSale component and it's elements

--- src/components/TokenSale/ChartBar.js
+++ src/components/TokenSale/ChartBar.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import classes from './ChartBar.module.css'
 
-const ChartBar = () => {
+const ChartBar = ({leftTitle, rightTitle, bar_data}) => {
   return (
     <>
-
-      <div className={classes.chart_bar_container}>
-        <div className={classes.chart_bar_progress}></div>
-        <span className={classes.chart_bar_point}></span>
+      <div className={classes.bar_title}>
+        <span><h3>{leftTitle.title} -</h3>{leftTitle.description}</span>
+        <span><h3>{rightTitle.title} -</h3>{rightTitle.description}</span>
+      </div>
+      <div className={classes.bar_container}>
+        <div className={classes.chart_bar_container}>
+          <div className={classes.chart_bar_progress}></div>
+          <span className={classes.chart_bar_point}></span>
+          <div className={classes.horizontal_line_container}>
+            <span className={classes.horizontal_line}><h3>{bar_data.start}</h3></span>
+            <span className={classes.horizontal_line}><h3>{bar_data.mid}</h3></span>
+            <span className={classes.horizontal_line}><h3>{bar_data.end}</h3></span>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/components/TokenSale/TokenSale.js
+++ src/components/TokenSale/TokenSale.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Timer from './Timer'
 import classes from './TokenSale.module.css'
 import ChartBar from './ChartBar'
+import LinkButton from '../LinkButton/LinkButton'
+import { BsNewspaper } from 'react-icons/bs'
 
 const TokenSale = () => {
-  const timerData = [{ id: '1', time: 0, text: 'Days' }, { id: '2', time: 0, text: 'Hours' }, { id: '3', time: 0, text: 'Minutes' }, { id: '1', time: 0, text: 'Seconds' }]
+
+  const [timeInterval, setTimeInterval] = useState([{
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  }
+  ])
+  const timerData = [{ id: '1', time: timeInterval.days, text: 'Days' }, { id: '2', time: timeInterval.hours, text: 'Hours' }, { id: '3', time: timeInterval.minutes, text: 'Minutes' }, { id: '4', time: timeInterval.seconds, text: 'Seconds' }]
+  const deadline = "September, 18, 2023";
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+    setTimeInterval((prevState) => {
+      return ({
+        ...prevState,
+        days: Math.floor(time / (1000 * 60 * 60 * 24)),
+        seconds: Math.floor((time / 1000) % 60),
+        hours: Math.floor((time / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((time / 1000 / 60) % 60)
+      })
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <h2 className={classes.token_sale_title}>Token sale end in :</h2>
       <div className={classes.token_sale_container}>
         {timerData.map((data) => <Timer key={data.id} time={data.time} text={data.text} />)}
       </div>
-      <ChartBar />
+      <ChartBar leftTitle={{ title: 'Raised', description: '1,450 Tokens' }} rightTitle={{ title: 'Target', description: '150,000 Tokens' }} bar_data={{ start: 'Soft cap', mid: 'Crowd sale', end: 'Hard cap' }} />
+      <div className={classes.footer}>
+        <LinkButton type='blue'> Buy Token Now</LinkButton>
+      </div>
     </>
   )
 }
